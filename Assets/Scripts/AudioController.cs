2253b3237cebe7bfafd20fbbba0142abc7964dc9
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2023-02-28 19:05:57 -0500

    Animation commit

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -10,47 +10,54 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
+private Animator anim;
 
-void Start() {
-    
+void Start() 
+{
+  anim = GetComponent<Animator>();
 }
 
 // Update is called once per frame
 void Update()
 {
-     if (Input.GetKeyDown(KeyCode.W))
-        {
-          musicSource.clip = musicClipOne;
-          musicSource.Play();
+    // play clip 1
+    if (Input.GetKeyDown(KeyCode.W))
+    {
+      musicSource.clip = musicClipOne;
+      musicSource.Play();
+      anim.SetInteger("State", 1);
+    }
 
-         }
+    // stop clip 1
+    if (Input.GetKeyUp(KeyCode.W))
+    {
+      musicSource.Stop();
+      anim.SetInteger("State", 0);
+    }
 
-     if (Input.GetKeyUp(KeyCode.W))
-        {
-          musicSource.Stop();
+    // play clip 2
+    if (Input.GetKeyDown(KeyCode.R))
+    {
+      musicSource.clip = musicClipTwo;
+      musicSource.Play();
+      anim.SetInteger("State", 2);
+    }
 
-         }
+    // stop clip 2
+    if (Input.GetKeyUp(KeyCode.R))
+    {
+      musicSource.Stop();
+      anim.SetInteger("State", 0);
+    }
 
-     if (Input.GetKeyDown(KeyCode.R))
-        {
-          musicSource.clip = musicClipTwo;
-          musicSource.Play();
-         }
-
-     if (Input.GetKeyUp(KeyCode.R))
-        {
-          musicSource.Stop();
-
-         }
-
-     if (Input.GetKeyDown(KeyCode.L))
-        {
-          musicSource.loop = true;
-         }
-
-     if (Input.GetKeyUp(KeyCode.L))
-        {
-          musicSource.loop = false;
-        }
-   }
+    if (Input.GetKeyDown(KeyCode.L))
+    {
+      musicSource.loop = true;
+    }
+    if (Input.GetKeyUp(KeyCode.L))
+    {
+      musicSource.loop = false;
+    }
+  }
+  
 }
